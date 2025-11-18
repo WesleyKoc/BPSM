5dade6670a9b90cb98d49ce0c79da727b06908fb
Wesley Koc <s2238539@bioinfmsc8.bio.ed.ac.uk>
2025-11-18 12:46:02 +0000

    lecture 19 exercise stuff

---
+++ Lecture19/exercisescript.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+import os
+
+ecoli = open("/localdisk/data/BPSM/Lecture19/ecoli.txt").read().replace('\n', '').upper()[0:50000]
+
+window = 1000
+
+at = []
+for start in range(len(ecoli) - window):
+    win = ecoli[start:start+window]
+    at.append((win.count('A') + win.count('T')) / window)
+
+plt.figure(figsize=(20,10))
+plt.plot(at, label="AT contents of ecoli genome")
+plt.ylabel('AT content (%)')
+plt.xlabel('Position on genome')
+plt.legend()
+plt.savefig("e.coliATcontent50000bases.png",transparent=True)
+plt.show()
+
+
